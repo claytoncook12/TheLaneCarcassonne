eca4abdb6df2314abe9a8384d3b08c961d0625d8
Clayton Cook <claytoncook12@gmail.com>
2018-05-22 12:14:43 -0400

    Delete unneeded sql query

--- app/QueryData.sql
+++
@@ -1,5 +0,0 @@
-select name, date, game_type, number, outcome, pts, num_players 
-from outcomes
-inner join players on players.id = outcomes.player_id
-inner join games on games.id = outcomes.games_id;
-
